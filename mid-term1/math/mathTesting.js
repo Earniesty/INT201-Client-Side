0f78337186ae3ae5d39f8fa2e728a748f409ea9c
FameNu <phuwamet.fame@gmail.com>
2023-09-09 17:17:22 +0700

    add new function on math

--- mid-term1/math/mathTesting.js
+++ mid-term1/math/mathTesting.js
@@ -1,6 +1,3 @@
-# **Math**
-    
-```jsx
 let x = 4.75;
     
 let rounded = Math.round(x); // Round to the nearest integer
@@ -12,22 +9,14 @@ let squareRoot = Math.sqrt(x); // Square root
 let random = Math.random();    // Random number between 0 and 1
 let pi = Math.PI
 let floatingTwoPoint = pi.toFixed(2) // 3.14 but still string
-```
-    
-### How to random number:
-    
-```jsx
-let rand = Math.floor(Math.random() * (max - min + 1)) + min
-let randDice = Math.floor(Math.random() * 6) + 1
-```
-    
-### **Parsing and Converting:**
-    
-```jsx
-let strNumber = "123.45";
-    
-let parsedNumber = parseFloat(strNumber);
-let integerPart = parseInt(strNumber); // Parses as an integer (123)
-    
-// parseInt change value with floating like use Math.floor()
-```
+
+console.log(rounded) // 5
+console.log(ceiling) // 5
+console.log(floor) // 4
+console.log(absolute) // 4.75
+console.log(power) // 22.5625
+console.log(squareRoot) // 2.179449471770337
+console.log(random) // 0.931596767633758
+console.log(pi) // 3.141592653589793
+console.log(floatingTwoPoint) // 3.14 in string
+console.log(parseFloat(floatingTwoPoint)) // 3.14 in number
