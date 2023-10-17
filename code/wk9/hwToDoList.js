6f3b7ebb84b7427811f4323080551060fd557f61
FameNu <110712106+FameNu@users.noreply.github.com>
2023-10-17 22:01:38 +0700

    update code hw

--- code/wk9/hwToDoList.js
+++ code/wk9/hwToDoList.js
@@ -8,18 +8,19 @@ class Todo {
         return {id: this.id, description: this.description}
     }
 
-    setDescription(description) {
-        this.description = description
+    setDescription(newDescription) {
+        this.description = newDescription
     }
 }
 
 function todoManagemnet() {
     let todos = []
-    let nextId = 0
 
     function addTodo(desc) {
-        const todo = new Todo(++nextId, desc)
-        return todos.push(todo.getTodo())
+        const id = todos.length + 1
+        const todo = new Todo(id, desc)
+        return todos.push(todo)
+        // return todos.push(todo.getTodo())
     }
 
     function findTodo(searchId) {
@@ -41,8 +42,9 @@ function todoManagemnet() {
     function removeTodo(removeId) {
         // s1 // after replace on todos, get new address
         // todos = todos.filter(({id}) => id !== removeId) 
+
         // s2 // same address of todos before splice, just remove element
-        const index = findIndexTodo(removeId)
+        const index = findIndexTodo(removeId) // if not found, index = -1
         if (index >= 0) {
             const removeElement = todos.splice(index, 1)
             const {id, description:desc} = removeElement[0]
@@ -64,19 +66,32 @@ function todoManagemnet() {
     }
 }
 
+// only class Todo
 const todoOne = new Todo(1, 'Good guy')
 console.log(todoOne)
+console.log(typeof todoOne)
 console.log(todoOne.getTodo())
 todoOne.setDescription('Hello, Guy')
 console.log(todoOne.getTodo())
 
+// Todo in management
 let manageTodo = todoManagemnet()
 console.log(manageTodo.addTodo('Hello world'))
 console.log(manageTodo.addTodo('Play Game'))
 console.log(manageTodo.getTodos())
+
+// find
 console.log(manageTodo.findTodo(3))
+
+// findIndex
 console.log(manageTodo.findIndexTodo(1))
 console.log(manageTodo.addTodo('Play Basketball'))
 console.log(manageTodo.getTodos())
+
+// remove
 manageTodo.removeTodo(2)
+manageTodo.removeTodo(4)
 console.log(manageTodo.getTodos())
+
+// set new description
+manageTodo.getTodos()[manageTodo.findIndexTodo(1)]
