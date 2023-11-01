79acd3350f5b2db71e92492670afe17b0b96721e
FameNu <110712106+FameNu@users.noreply.github.com>
2023-11-01 21:01:55 +0700

    new module Liabary

---
+++ code/wk11/libary.js
@@ -0,0 +1,47 @@
+class Library {
+    constructor() {
+        this.books = []
+    }
+
+    getAllBooks() {
+        return this.books
+    }
+
+    addBook(title, author, year, genre) {
+        if (!title || !author || !year || !genre) return undefined
+        if (this.books.length > 0) {
+            for (const book of this.books) {
+                if (book.title.toLowerCase() === title.toLowerCase() &&
+                this.author.toLowerCase() === author.toLowerCase()) return undefined
+            }
+        }
+        const newBook = {
+            title,
+            author,
+            year,
+            genre
+        }
+        this.books.push(newBook)
+        return newBook
+    }
+
+    updateBook(title, updateDetail) {
+        const findIndex = this.books.findIndex(({title:findTitle}) => findTitle.toLowerCase() === title.toLowerCase())
+        if (findIndex === -1) return undefined
+        const updateBook = {
+            title,
+            ...updateDetail
+        }
+        this.books[findIndex] = updateBook
+        return updateBook
+    }
+
+    deleteBookByTitle(title) {
+        const deleteIndex = this.books.findIndex(({title:findTitle}) => findTitle.toLowerCase() === title.toLowerCase())
+        if (deleteIndex === -1) return `not found ${title}`
+        this.books.splice(deleteIndex, 1)
+        return `delete "${title}" sucess`
+    }
+}
+
+module.exports = {Library}
---
+++ code/wk11/useit.js
@@ -0,0 +1,25 @@
+const {Library} = require('./libary.js')
+
+const library = new Library()
+
+console.log(library.addBook('The Great Gatsby', 'F. Scott Fitzgerald', 1925, 'Fiction'))
+console.log(library.addBook('To Kill a Mockingbird', 'Harper Lee', 1960, 'Fiction'))
+console.log(library.addBook('Moby-Dick', 'Herman Melville', 1851, 'Adventure'))
+
+console.log(library.getAllBooks())
+
+console.log(library.updateBook('The Great Gatsby', { author: 'Ernest Hemingway' }))
+
+console.log(library.deleteBookByTitle('Moby-Dick'))
+// Should delete the book 'Moby-Dick' from the library.
+
+console.log(library.addBook('To Kill a Mockingbird', 'Harper Lee', 1960, 'Fiction'))
+// Should return undefined since a book with the same title and author already exists.
+
+console.log(library.addBook('Harry Potter and the Sorcerer\'s Stone', 'J.K. Rowling', 1997))
+// Should return undefined since the genre is missing.
+
+console.log(library.updateBook('The Catcher in the Rye', { year: 1951 }))
+// Should return undefined since 'The Catcher in the Rye' is not in the library.
+
+console.log(library.deleteBookByTitle('The great gatsby'))
