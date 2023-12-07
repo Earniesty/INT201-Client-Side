4a5b3240c6487d70666a8f592f49777639593fda
FameNu <110712106+FameNu@users.noreply.github.com>
2023-12-07 11:33:12 +0700

    edit validate on my practice

--- code/wk13/main.js
+++ code/wk13/main.js
@@ -1,34 +1,34 @@
-// const submitButton = document.querySelector('button')
-// console.log(submitButton)
-// const doSomething = (e) => { // รับ evnet จาก addEventListener()
-//     // console.log('Submit clicked')
-//     // console.log(e)
-//     // console.log(e.target.nodeName)
-//     console.log(e.target.id)
-//     // console.log(e.type)
-//     // console.log(e.target)
-//     console.log(e.currentTarget)
-//     console.log(e.eventPhase)
-// }
-// submitButton.addEventListener('click', doSomething)
+const submitButton = document.querySelector('button')
+console.log(submitButton)
+const doSomething = (e) => { // รับ evnet จาก addEventListener()
+    // console.log('Submit clicked')
+    // console.log(e)
+    // console.log(e.target.nodeName)
+    console.log(e.target.id)
+    // console.log(e.type)
+    // console.log(e.target)
+    console.log(e.currentTarget)
+    console.log(e.eventPhase)
+}
+submitButton.addEventListener('click', doSomething)
 
 // ------------------------------------------------------------------------------
-// const innerDiv = document.getElementById('inner-div')
-// console.log(innerDiv)
-// innerDiv.addEventListener('click', (event) => {
-//     // console.log(event.target)
-//     console.log(event.target.id)
-//     console.log(event.currentTarget)
-//     console.log(event.eventPhase)
-// })
+const innerDiv = document.getElementById('inner-div')
+console.log(innerDiv)
+innerDiv.addEventListener('click', (event) => {
+    // console.log(event.target)
+    console.log(event.target.id)
+    console.log(event.currentTarget)
+    console.log(event.eventPhase)
+})
 
-// // outerDiv
-// const outerDiv = document.getElementById('outer-div')
-// outerDiv.addEventListener('click', (event) => {
-//     console.log(event.target.id)
-//     console.log(event.currentTarget)
-//     console.log(event.eventPhase)
-// })
+// outerDiv
+const outerDiv = document.getElementById('outer-div')
+outerDiv.addEventListener('click', (event) => {
+    console.log(event.target.id)
+    console.log(event.currentTarget)
+    console.log(event.eventPhase)
+})
 
 // --------------------- after break time
 // const submitButton = document.querySelector('button')
@@ -44,70 +44,70 @@
 // submitButton.removeEventListener('click', doSomething)
 
 // second.html
-const submitButton = document.querySelector('button')
+// const submitButton = document.querySelector('button')
 
-submitButton.addEventListener('click', (e) => {
-    console.log(e.preventDefault()) // undefined
-    // console.log('cancle submit to server')
+// submitButton.addEventListener('click', (e) => {
+//     console.log(e.preventDefault()) // undefined
+//     // console.log('cancle submit to server')
 
-    const userInput = document.getElementById('input-user')
-    const username = userInput.value.trim()
+//     const userInput = document.getElementById('input-user')
+//     const username = userInput.value.trim()
 
-    const pswInput = document.getElementById('input-psw')
-    const password = pswInput.value.trim()
+//     const pswInput = document.getElementById('input-psw')
+//     const password = pswInput.value.trim()
 
-    const showBox = document.querySelector('div>p')
-    if (!username) showBox.innerHTML = `username empty`
-    if (!password) showBox.innerHTML = `password empty`
-    if (!password && !username) showBox.innerHTML = `username and password empty`
-    if (password && username) showBox.innerHTML = `username: ${username}, password: ${password}`
-})
-
-
-// window document
-// document.addEventListener('DOMContentLoaded', () => {
-//     console.log('Your DOM tree built')
+//     const showBox = document.querySelector('div>p')
+//     if (!username) showBox.innerHTML = `username empty`
+//     if (!password) showBox.innerHTML = `password empty`
+//     if (!password && !username) showBox.innerHTML = `username and password empty`
+//     if (password && username) showBox.innerHTML = `username: ${username}, password: ${password}`
 // })
 
-// window.addEventListener('load', () => {
-//     console.log('Your application loaded')
+
+// // window document
+// // document.addEventListener('DOMContentLoaded', () => {
+// //     console.log('Your DOM tree built')
+// // })
+
+// // window.addEventListener('load', () => {
+// //     console.log('Your application loaded')
+// // })
+
+// // window.addEventListener('beforeunload', () => {
+// //     localStorage.setItem('myCat', 'Tom')
+// // })
+
+// // window.addEventListener('mousemove', (e) => {
+// //     console.log(`x:${e.screenX}, y:${e.screenY}`)
+// // })
+
+// const userElement = document.getElementById('input-user')
+// const pswElement = document.getElementById('input-psw')
+
+// userElement.addEventListener('focus', () => {
+//     console.log('Your focus is on your username')
 // })
 
-// window.addEventListener('beforeunload', () => {
-//     localStorage.setItem('myCat', 'Tom')
+// userElement.addEventListener('blur', () => {
+//     console.log('Your username input textbox is blured')
 // })
 
-// window.addEventListener('mousemove', (e) => {
-//     console.log(`x:${e.screenX}, y:${e.screenY}`)
+// userElement.addEventListener('keydown', (e) => {
+//     console.log(`${e.key}, ${e.code}`)
+//     if (e.key >= 0 && e.key <= 9) e.preventDefault()
 // })
 
-const userElement = document.getElementById('input-user')
-const pswElement = document.getElementById('input-psw')
+// // when user release enter key, text value on username will show at <p></p> element
+// userElement.addEventListener('keyup', (e) => {
+//     if (e.key === 'Enter') {
+//         const pElement = document.querySelector('p')
+//         pElement.textContent = e.target.value
+//     }
+// })
 
-userElement.addEventListener('focus', () => {
-    console.log('Your focus is on your username')
-})
-
-userElement.addEventListener('blur', () => {
-    console.log('Your username input textbox is blured')
-})
-
-userElement.addEventListener('keydown', (e) => {
-    console.log(`${e.key}, ${e.code}`)
-    if (e.key >= 0 && e.key <= 9) e.preventDefault()
-})
-
-// when user release enter key, text value on username will show at <p></p> element
-userElement.addEventListener('keyup', (e) => {
-    if (e.key === 'Enter') {
-        const pElement = document.querySelector('p')
-        pElement.textContent = e.target.value
-    }
-})
-
-userElement.addEventListener('input', (e) => {
-    const pElement = document.querySelector('p')
-    pElement.textContent = e.target.value
-})
+// userElement.addEventListener('input', (e) => {
+//     const pElement = document.querySelector('p')
+//     pElement.textContent = e.target.value
+// })
 
 
